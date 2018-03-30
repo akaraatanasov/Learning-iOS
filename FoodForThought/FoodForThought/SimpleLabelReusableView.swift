d82f2a5e63c698029647b31127425a8cd691312c
Alexander Karaatanasov <akaraatanasov@gmail.com>
2018-03-30 19:22:15 +0300

    Initial commit
    All my Xcode projects

---
+++ FoodForThought/FoodForThought/SimpleLabelReusableView.swift
@@ -0,0 +1,15 @@
+//
+//  FoodHeaderReusableView.swift
+//  FoodForThought
+//
+//  Created by CS193p Instructor.
+//  Copyright © 2017 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class SimpleLabelReusableView: UICollectionReusableView {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
